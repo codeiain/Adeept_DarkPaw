ce84d86a02c879550c5b28228d81ab025fcddb9e
Iain Brown <codeiain@outlook.com>
2024-11-22 22:51:30 +0000

    add web ui

---
+++ adeept_bot_controller_web/src/components/controll/ArmControlMod.vue
@@ -0,0 +1,30 @@
+<template>
+  <ButtonsChild :buttons="buttons" :cols="cols"/>
+</template>
+
+<script>
+import ButtonsChild from '@/components/controll/child/ButtonsChild.vue'
+
+export default ({
+  name: 'ArmControlMod',
+  components: {
+    ButtonsChild
+  },
+  data: () => ({
+    buttons: [
+      [false, 'Grab', 'grab', 85, 'stop'],
+      [false, 'Up', 'up', 73, 'UDstop'],
+      [false, 'Loose', 'loose', 79, 'stop'],
+      [false, 'HandUp', 'handup', 80, 'HAstop'],
+      [false, 'Left', 'lookleft', 74, 'LRstop'],
+      [false, 'Down', 'down', 75, 'UDstop'],
+      [false, 'Right', 'lookright', 76, 'LRstop'],
+      [false, 'HandDown', 'handdown', 186, 'HAstop']
+    ],
+    cols: 4
+  })
+})
+</script>
+
+<style lang="stylus" scoped>
+</style>
